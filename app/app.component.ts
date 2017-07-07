f143886fea0aaacf22ba97ecb82b4b75b8b64769
Brian Rush <yankeh@gmail.com>
2017-07-06 18:34:17 -0700

    follow along with the tutorial through https://www.learnhowtoprogram.com/javascript/angular-js/template-reference-variables-and-creating-objects , one more to go

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,21 @@ import { Task } from './task.model';
 @Component({
   selector: 'app-root',
   template: `
+  <!-- <div class="JUMBOTRON CONTAINER">
+    <p>
+      WILL THIS SHOW??
+    </p>
+  </div> -->
       <div class="container">
         <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
         <h3>{{currentFocus}}</h3>
         <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
         <hr>
         <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+        <new-task (newTaskSender)="addTask($event)"></new-task>
       </div>
-    `
-})
-
+      `
+    })
 
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
@@ -35,6 +40,10 @@ export class AppComponent {
 
   finishedEditing() {
     this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 
 }
